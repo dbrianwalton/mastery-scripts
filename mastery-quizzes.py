d762ffea36bde7f53d658a614b45d02059ea39b5
D Brian Walton <dbrianwalton@gmail.com>
2020-06-06 14:08:07 -0400

    Improve documentation in mastery-quizzes script

--- mastery-quizzes.py
+++ mastery-quizzes.py
@@ -3,15 +3,17 @@ import argparse
 import subprocess
 
 parser = argparse.ArgumentParser(description='Import a Canvas mastery export file and produce a summary.')
-parser.add_argument('--csv', dest='csvFile')
-parser.add_argument('--outcomes', dest='outcomeFile')
-parser.add_argument('--quiz', dest='quizInclude')
-parser.add_argument('--quizDir')
-parser.add_argument('--week', dest='week', type=int, default=0)
-parser.add_argument('--generic')
-parser.add_argument('--students', dest='studentData', default='')
+parser.add_argument('--csv', dest='csvFile', help='file path, CSV of Canvas learning mastery report')
+parser.add_argument('--outcomes', dest='outcomeFile', help='file path, text file with 4 columns: group code (text), outcome code (text), latex command stem (only alpha), week introduced (integer)')
+parser.add_argument('--quiz', dest='quizInclude', help='file name, latex source for quiz generation')
+parser.add_argument('--quizDir', help='folder path, where student quizzes are saved')
+parser.add_argument('--week', dest='week', type=int, default=0, help='integer, include all weeks up to this value')
+parser.add_argument('--students', dest='studentData', default='', help='file path, CSV of Canvas grade export to get section information')
+parser.add_argument('--doneScore', type=int, default=3, help='integer, skip problems of outcomes if already achieve this')
+parser.add_argument('--apprenticeScore', type=int, default=2, help='integer, mark problems of outcomes if at this level')
 args = parser.parse_args()
 
+# Some structure to keep track of outcomes, organized by groups.
 class Outcome:
     def __init__(self, groupCode, groupTitle, outcomeCode, outcomeTitle, index):
         self.groupCode = groupCode
@@ -22,6 +24,7 @@ class Outcome:
     def codeStr(self):
         return (getOutcomeCode(self.groupCode, self.outcomeCode))
 
+# Some structure to keep track of who a student is and what they have done.
 class StudentRecord:
     def __init__(self, student_name, student_id, student_results):
         self.name = student_name
@@ -34,8 +37,15 @@ class StudentRecord:
         names = self.name.split(' ')
         return ''.join([names[-1], ''.join(names[:-1])])
 
+# Groups are where learning outcomes are organized
+# On Canvas, I have learning outcomes grouped into a hierarchy
+# Each group has a simple code and a descriptor
+# For example "G1: Functions"
+# The script uses the G1 as the group code and Functions as the title.
+# Similarly, an outcome has a code and a title in a similar vein.
+# For example "F1: Defining Functions"
+# The outcome code would be F1 and the title would be Defining Functions
 groups = dict()
-
 outcomeArray = []
 outcomeDict = dict()
 def addOutcome(outcome):
@@ -46,11 +56,15 @@ def addOutcome(outcome):
     group['outcomes'].append(outcome.outcomeCode)
     groups[outcome.groupCode] = group
 
+# In the script, outcomes have both a group and outcomes
+# This allows us to sort by groups if desired.
+# Using the example above, this function would return "G1.F1" as the outcome code.
 def getOutcomeCode(group, outcome):
     return '.'.join([group, outcome])
 
 # Parse the first row of the data table
 # Extracts the information for outcome descriptions
+# My Outcome Titles include the Outcome and Title, so we pull this off for tracking.
 def parseHeader(headers, numberOutcomes):
     # Canvas exports outcome information in the headers.
     # Column 1: Student Name
@@ -80,6 +94,7 @@ def parseHeader(headers, numberOutcomes):
         addOutcome(outcome)
 
 # Parse one row of the table corresponding to a student's mastery record
+# The ordering of columns is described above in parseHeader
 def parseRow(studentRecord, numOutcomes):
     # Column 1: name
     name = studentRecord[0]
@@ -101,11 +116,13 @@ def parseRow(studentRecord, numOutcomes):
             required = 0.0
         else:
             required = float(studentRecord[k+1])
-        results.append({ 'score': score, 'mastery': (score >= required) })
+        results.append({ 'score': score, 'mastery': (score >= args.doneScore) })
 
     record = StudentRecord(name, id, results)
     return record
 
+# I like to have a template quiz generated.
+# So this creates the fake student where that will be used.
 def BlankStudent(numOutcomes):
     results = []
     for i in range(numOutcomes):
@@ -114,71 +131,24 @@ def BlankStudent(numOutcomes):
     record = StudentRecord('Problem Template', '', results)
     return record
 
-# Use the records for the student and the outcomes to generate a report
-def generateReport(reportFile, studentRecord):
-    # Display student header information
-    reportFile.write(studentRecord.name)
-    reportFile.write('\n')
-    numMastered = 0
-
-    def addGroupHeader(groupCode):
-        # Display group header information
-        group = groups[groupCode]
-        reportFile.write(''.join([groupCode, ': ', group['title']]))
-        reportFile.write('\n')
-
-    groupCodes = sorted(groups.keys())
-    for groupCode in groupCodes:
-        # Display group header information when outcome appears.
-        needGroupHeader = True
-        group = groups[groupCode]
-
-        # Go through the outcomes from this group.
-        outcomeCodes = sorted(group['outcomes'])
-        for outcomeCode in outcomeCodes:
-            code = getOutcomeCode(groupCode, outcomeCode)
-            # See if this outcome is to be included
-            if code in useOutcomeDict:
-                # Is this the first included outcome for the group?
-                if needGroupHeader:
-                    addGroupHeader(groupCode)
-                    needGroupHeader = False
-
-                # Now generate the output for this outcome.
-                # Also update the overall statistics for the outcome
-                outcome = outcomeDict[code]
-                outcomeStat = outcomeStats[code]
-                progress = ''
-                if studentRecord.results[outcome.index]['mastery']:
-                    progress = 'Mastered'
-                    numMastered = numMastered + 1
-                    outcomeStat[0] = outcomeStat[0] + 1
-                else:
-                    outcomeStat[1] = outcomeStat[1] + 1
-                outcomeStats[code] = outcomeStat
-
-                reportFile.write(''.join(['  ', outcomeCode, ' ', outcome.outcomeTitle,': ', progress]))
-                reportFile.write('\n')
-    reportFile.write('Total Number of Mastered Objectives: ' + str(numMastered) + '\n')
-    reportFile.write('\n\n----------------\n\n')
-
-def generateStatisticsReport(reportFile):
-    groupCodes = sorted(groups.keys())
-    for groupCode in groupCodes:
-        # Display group header information when outcome appears.
-        needGroupHeader = True
-        group = groups[groupCode]
-
-        # Go through the outcomes from this group.
-        outcomeCodes = sorted(group['outcomes'])
-        for outcomeCode in outcomeCodes:
-            code = getOutcomeCode(groupCode, outcomeCode)
-            if code in useOutcomeDict:
-                stats = outcomeStats[code]
-                reportFile.write("%s: Passed = %d, Not Yet=%d\n" % (code, stats[0], stats[1]))
-
 # Use the records for the student and the outcomes to generate a new quiz
+# The LaTeX file in args.quizInclude needs to have in the preamble
+# a definition of every problem as a \newcommand.
+# To match outcomes, the OutcomeList.txt file in args.outcome
+# defines a LaTeX stem for each outcome. The command defined in the LaTeX
+# adds a prefix \obj. The stem can not include numeric symbols, so I use Roman
+# numerals. For example, "F1" would have a command \objFI and "FI" would be in
+# the 3rd column of the Mastery Outcomes text file.
+# In the body of the LaTeX file, we need to include the "Questions.tex" file.
+# So here is the document body:
+#\begin{document}
+#\pagestyle{myheadings}
+#\raggedbottom
+#\include{Questions}
+#\end{document}
+#
 def generateQuiz(studentRecord):
+    # Create a personalized quiz for each student.
     with open('Questions.tex', 'w') as quizFile:
         # Display student header information
         quizFile.write('\\setcounter{page}{1}\n\\markright{')
@@ -200,9 +170,10 @@ def generateQuiz(studentRecord):
             if studentRecord.results[outcome.index]['mastery']:
                 masteredList.append(outcome.outcomeCode)
 
-        quizFile.write('\\objMastery{')
-        quizFile.write(', '.join(masteredList))
-        quizFile.write('}\n\n')
+        if len(masteredList) > 0:
+            quizFile.write('\\objMastery{')
+            quizFile.write(', '.join(masteredList))
+            quizFile.write('}\n\n')
 
         # Then add all of the problems not mastered.
         for outcomeRow in useOutcomes:
@@ -211,7 +182,10 @@ def generateQuiz(studentRecord):
             if not studentRecord.results[outcome.index]['mastery']:
                 quizFile.write('\\obj')
                 quizFile.write(outcomeRow[2])
-                quizFile.write('{}\n')
+                if studentRecord.results[outcome.index]['score'] == args.apprenticeScore:
+                    quizFile.write('{ (A)}\n')
+                else:
+                    quizFile.write('{}\n')
                 masteredList.append(outcome.outcomeCode)
         quizFile.write('\\end{enumerate}\n\n\\cleardoublepage \n\n')
     # Now that the problems were generated, run pdflatex to create the quiz.
@@ -221,6 +195,8 @@ def generateQuiz(studentRecord):
         '-output-directory', args.quizDir,
         args.quizInclude])
 
+# Here is where the real work takes place.
+# First load the file containing mastery data from Canvas report export.
 # Parse the data file to create our desired information
 with open(args.csvFile, newline='') as masteryFile:
     # Create an iterator to go through the rows on the file.
@@ -235,6 +211,8 @@ with open(args.csvFile, newline='') as masteryFile:
     # All other rows are individual student records
     studentData = [ parseRow(row, numberOutcomes) for row in dataStream ]
 
+# Second, read information about students and which sections they are in.
+# It gets printed in the header for convenience on paper copies
 # Parse the student data file to organize the printing
 if (args.studentData != ''):
     studentSections = dict()
@@ -251,6 +229,13 @@ if (args.studentData != ''):
             studentSections[name] = section
 
 # Parse the restricted set of outcomes that will be included.
+# This is reading the tab-delimited text file summarizing outcome information.
+# GroupCode \t OutcomeCode \t LateX_stem \t Week_Introduced
+# An example line might be as follows
+# G1 \t F1 \t FI \t 2
+# This would mean that outcome F1 from group G1 began assessment on quiz 2 (week)
+# Further, the LaTeX quiz is using \objFI as the command defining the problem
+# that will be included.
 useOutcomes = []
 useOutcomeDict = dict()
 outcomeStats = dict()
@@ -264,6 +249,7 @@ with open(args.outcomeFile, 'r') as outcomeFile:
             outcomeStats[objCode] = [0, 0] # pass/not yet
             useOutcomes.append(row)
 
+# We will generate the quizzes in student order
 # Create a sort order for students base on LastName, FirstName
 def nameKey(i):
     return studentData[i].getLastFirst()
@@ -280,4 +266,5 @@ if (args.studentData != ''):
 
 for i in order:
     generateQuiz(studentData[i])
+# The template quiz will be generated last.
 generateQuiz(BlankStudent(numberOutcomes))
